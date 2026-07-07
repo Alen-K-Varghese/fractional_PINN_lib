494624490ae94e7d71bdab0fe52ef657e8c1dbcc
Alen-K-Varghese <alenkvarghese04@gmail.com>
2026-07-07 19:55:37 +0530

    tensor operations

--- tensor.hpp
+++ tensor.hpp
@@ -8,6 +8,7 @@
 #include<cassert>
 #include <cstddef>
 #include <functional>
+#include <iostream>
 #include <type_traits>
 
 namespace ff {
@@ -15,6 +16,9 @@ namespace ff {
 namespace core {
 
     constexpr std::size_t max_dims = 4;
+    template<typename dtype, size_t... shape_>
+    struct Tensor;
+
 
     template<typename T>
     constexpr bool valid_index_type =
@@ -25,15 +29,14 @@ namespace core {
         !std::is_same_v<std::__remove_cvref_t<T>, unsigned char>;
 
 
-    template<typename dtype, size_t... shape>
-    struct Tensor;
+
 
     // Dependency for the tensor (for autograd)
-    template<typename dtype, size_t... shape>
+    template<typename dtype, size_t... shape_>
     struct Dependency{
-    Tensor<dtype, shape...> *dependant_tensor;
+    Tensor<dtype, shape_...> *dependant_tensor;
     std::function<
-            Tensor<dtype, shape...>(Tensor<dtype, shape...>)> assosiated_grad_fcn;
+            Tensor<dtype, shape_...>(Tensor<dtype, shape_...>)> assosiated_grad_fcn;
     };
 
     // Main Tensor struct 
@@ -41,21 +44,37 @@ namespace core {
     struct Tensor{
 private:
         // Number of dimensions (fixed at compile time)
-        static constexpr size_t n_dim_ = sizeof...(shape);
+        static constexpr size_t n_dim_ = []{
+            constexpr size_t tmp_n_dims = sizeof...(shape);
+            constexpr std::array<size_t, tmp_n_dims> temp_{shape...};
+
+            size_t actual_dims = tmp_n_dims;
+            for(size_t i = 0; i < tmp_n_dims; ++i){
+                if(temp_[i] == 1){--actual_dims;}
+            }
+            return std::max(actual_dims, size_t{0});
+        }();
+        static_assert(n_dim_<= max_dims, "Max dimensions allowed: 4" );
 
         // Number of elements (fixed at compile time)
         static constexpr size_t num_elems_ = std::max((1*...*shape), size_t{1});
 
-        static_assert(n_dim_<= max_dims, "Max dimensions allowed: 4" );
+        // The tensor data storage
+        std::array<dtype, num_elems_> data_ = {};
 
+public:
+        static constexpr size_t size = num_elems_;
 
         static constexpr std::array<size_t, max_dims> shape_ = []{
             std::array<size_t, max_dims> shape_arr = {};
-            constexpr std::array<size_t, n_dim_> temp_{shape...};
 
-            for(size_t i = 0; i < n_dim_; ++i) shape_arr[max_dims - n_dim_ + i] = temp_[i];
+            constexpr size_t tmp_n_dims = sizeof...(shape);
+            constexpr std::array<size_t, tmp_n_dims> temp_ = {shape...};
+
+
+            for(size_t i = 0; i < tmp_n_dims; ++i) shape_arr[max_dims - tmp_n_dims + i] = temp_[i];
             for(size_t j = 0; j < max_dims; j++) {
-                if (shape_arr[j] == 0) shape_arr[j] = 1;
+                if (shape_arr[j] == 0) {shape_arr[j] = 1;}
             }
 
             return shape_arr;
@@ -64,54 +83,28 @@ private:
         // Lmabda for getting stride
         static constexpr std::array<size_t, max_dims> stride_ = []{
             std::array<size_t, max_dims> tmp_arr = {};
-            tmp_arr[max_dims - 1] = size_t{1};
-            for(size_t i = max_dims-2; i >= max_dims - n_dim_; --i) tmp_arr[i] = tmp_arr[i+1] * shape_[i+1];
+                if(n_dim_ > 0){
+                    tmp_arr[max_dims - 1] = size_t{1};
+                    for(size_t i = max_dims-2; i >= max_dims - n_dim_; --i) tmp_arr[i] = tmp_arr[i+1] * shape_[i+1];
+                }
 
             return tmp_arr;
         }();
 
-        // The tensor data storage
-        std::array<dtype, num_elems_> data_ = {};
-
-        // Declaration of helper functions as friend
-        template<typename dtype_ret, size_t... shape_ret,
-                typename dtype_ten1, size_t... shape_ten1,
-                typename dtype_ten2, size_t... shape_ten2>
-        
-        friend bool add_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                        core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                        core::Tensor<dtype_ten2, shape_ten2...>* ten2);
-        template<typename dtype_ret, size_t... shape_ret,
-                typename dtype_ten1, size_t... shape_ten1,
-                typename dtype_ten2, size_t... shape_ten2>
-        
-        bool substract_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                        core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                        core::Tensor<dtype_ten2, shape_ten1...>* ten2);
-
-        template<typename dtype_ret, size_t... shape_ret,
-                typename dtype_ten1, size_t... shape_ten1,
-                typename dtype_ten2, size_t... shape_ten2>
-        
-        bool mult_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                        core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                        core::Tensor<dtype_ten2, shape_ten1...>* ten2);
-public:
         // Constructor
         Tensor(std::array<dtype, num_elems_> const &data)
         :data_(data)
-        {}
-
-        // Getting the shape of the tensor
-        std::array<dtype, n_dim_> get_shape(){ return shape_; }
-
-        // Getting the number of elements in the tensor
-        size_t get_size(){return num_elems_;}
+        {std::cout<<" "<<std::endl;std::cout<<n_dim_<<std::endl;}
 
         // direct indexing to internal data array. Hepls in broadcasting operations
         dtype at_flat(size_t idx){ 
             assert(idx < num_elems_);
             return data_[idx];}
+
+        void assign_at_flat(size_t idx, dtype element){
+            assert(idx < num_elems_);
+            data_[idx] = element;
+        }
 
         template<typename Idx0, typename... Index_types> 
         requires (
@@ -142,39 +135,93 @@ public:
 
 
 }
-    template<typename dtype_ret, size_t... shape_ret,
-            typename dtype_ten1, size_t... shape_ten1,
-            typename dtype_ten2, size_t... shape_ten2>
-    
-    bool add_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                    core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                    core::Tensor<dtype_ten2, shape_ten2...>* ten2){
-                        assert(RET->num_elems_ == std::max(ten1->num_elems_, ten2->num_elems_));
+namespace tensor_op {
 
+    template<typename dtype, size_t... shape_ret, size_t... shape_ten1, size_t... shape_ten2>
+    bool add_tensor(core::Tensor<dtype, shape_ret...>* RET, 
+                    core::Tensor<dtype, shape_ten1...>* ten1, 
+                    core::Tensor<dtype, shape_ten2...>* ten2){
+                        if (RET->size != std::max(ten1->size, ten2->size)){return false;}
+                        for (size_t j = 0; j < core::max_dims; j ++) {
+                            if(RET->shape_[j] != std::max(ten1->shape_[j], ten2->shape_[j])){return false;}
+                        }
+                        for(size_t i = 0; i < RET->shape_[0]; ++i){
+                            for(size_t j = 0; j < RET->shape_[1]; ++j){
+                                for(size_t k = 0; k < RET->shape_[2]; ++k){
+                                    for(size_t l = 0; l < RET->shape_[3]; ++l){
+                                        dtype element = 
+                                            ten1->at_flat(ten1->stride_[0]*i + ten1->stride_[1]*j + ten1->stride_[2]*k + ten1->stride_[3]*l)
+                                        +   ten2->at_flat(ten2->stride_[0]*i + ten2->stride_[1]*j + ten2->stride_[2]*k + ten2->stride_[3]*l);
 
+                                        std::cout<<i<<j<<k<<l;
+                                        std::cout<<element<<std::endl;
+
+                                        RET->assign_at_flat(RET->stride_[0]*i + RET->stride_[1]*j + RET->stride_[2]*k + RET->stride_[3]*l, element);
+                                    }
+                                }
+                            }
+                        }
+
+                        return true;
                     }
 
-    template<typename dtype_ret, size_t... shape_ret,
-            typename dtype_ten1, size_t... shape_ten1,
-            typename dtype_ten2, size_t... shape_ten2>
-    
-    bool substract_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                    core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                    core::Tensor<dtype_ten2, shape_ten1...>* ten2){
-                        assert(RET->num_elems_ == std::max(ten1->num_elems_, ten2->num_elems_));
+    template<typename dtype, size_t... shape_ret, size_t... shape_ten1, size_t... shape_ten2>
+    bool subdtract_tensor(core::Tensor<dtype, shape_ret...>* RET, 
+                    core::Tensor<dtype, shape_ten1...>* ten1, 
+                    core::Tensor<dtype, shape_ten2...>* ten2){
+                        if (RET->size != std::max(ten1->size, ten2->size)){return false;}
+                        for (size_t j = 0; j < core::max_dims; j ++) {
+                            if(RET->shape_[j] != std::max(ten1->shape_[j], ten2->shape_[j])){return false;}
+                        }
+                        for(size_t i = 0; i < RET->shape_[0]; ++i){
+                            for(size_t j = 0; j < RET->shape_[1]; ++j){
+                                for(size_t k = 0; k < RET->shape_[2]; ++k){
+                                    for(size_t l = 0; l < RET->shape_[3]; ++l){
+                                        dtype element = 
+                                            ten1->at_flat(ten1->stride_[0]*i + ten1->stride_[1]*j + ten1->stride_[2]*k + ten1->stride_[3]*l)
+                                        -   ten2->at_flat(ten2->stride_[0]*i + ten2->stride_[1]*j + ten2->stride_[2]*k + ten2->stride_[3]*l);
 
+                                        std::cout<<i<<j<<k<<l;
+                                        std::cout<<element<<std::endl;
+
+                                        RET->assign_at_flat(RET->stride_[0]*i + RET->stride_[1]*j + RET->stride_[2]*k + RET->stride_[3]*l, element);
+                                    }
+                                }
+                            }
+                        }
+
+                        return true;
                     }
 
-    template<typename dtype_ret, size_t... shape_ret,
-            typename dtype_ten1, size_t... shape_ten1,
-            typename dtype_ten2, size_t... shape_ten2>
-    
-    bool add_tensor(core::Tensor<dtype_ret, shape_ret...>* RET, 
-                    core::Tensor<dtype_ten1, shape_ten1...>* ten1, 
-                    core::Tensor<dtype_ten2, shape_ten1...>* ten2){
-                        assert(RET->num_elems_ == std::max(ten1->num_elems_, ten2->num_elems_));
-                    
+    template<typename dtype, size_t... shape_ret, size_t... shape_ten1, size_t... shape_ten2>
+    bool mult_tensor(core::Tensor<dtype, shape_ret...>* RET, 
+                    core::Tensor<dtype, shape_ten1...>* ten1, 
+                    core::Tensor<dtype, shape_ten2...>* ten2){
+                        if (RET->size != std::max(ten1->size, ten2->size)){return false;}
+                        for (size_t j = 0; j < core::max_dims; j ++) {
+                            if(RET->shape_[j] != std::max(ten1->shape_[j], ten2->shape_[j])){return false;}
+                        }
+                        for(size_t i = 0; i < RET->shape_[0]; ++i){
+                            for(size_t j = 0; j < RET->shape_[1]; ++j){
+                                for(size_t k = 0; k < RET->shape_[2]; ++k){
+                                    for(size_t l = 0; l < RET->shape_[3]; ++l){
+                                        dtype element = 
+                                            ten1->at_flat(ten1->stride_[0]*i + ten1->stride_[1]*j + ten1->stride_[2]*k + ten1->stride_[3]*l)
+                                        *   ten2->at_flat(ten2->stride_[0]*i + ten2->stride_[1]*j + ten2->stride_[2]*k + ten2->stride_[3]*l);
+
+                                        std::cout<<i<<j<<k<<l;
+                                        std::cout<<element<<std::endl;
+
+                                        RET->assign_at_flat(RET->stride_[0]*i + RET->stride_[1]*j + RET->stride_[2]*k + RET->stride_[3]*l, element);
+                                    }
+                                }
+                            }
+                        }
+
+                        return true;
                     }
+                }
+
 }
 
 #endif
--- test/tensor_test.cpp
+++ test/tensor_test.cpp
@@ -1,14 +1,27 @@
 #include "../tensor.hpp"
 #include <iostream>
+#include <ostream>
+
 
 int main()
 {
     ff::core::Tensor<float, 2, 2> a_ten({1.0, 2.0, 3.0, 4.0});
+    ff::core::Tensor<float, 2, 2> b_ten({2.0, 3.0, 4.0, 5.0});
+    ff::core::Tensor<float, 1, 2> c_ten({10.0, 15.0});
 
-    float b = (float)a_ten(0, 4);
+    ff::core::Tensor<float, 2, 2> ret_ten({0.0, 0.0, 0.0, 0.0});
+
+    for(int i = 0; i < ff::core::max_dims; i ++){
+        std::cout<<c_ten.stride_[i];}
+    std::cout<<" "<<std::endl;
+    
+    if (ff::tensor_op::add_tensor(&ret_ten, &a_ten, &c_ten)){
+        for(size_t i = 0; i < 4; i++) std::cout<<ret_ten.at_flat(i)<<std::endl;
+    }
+
+    float b = (float)a_ten(0, 1);
 
     std::cout<<b<<std::endl;
-    std::cout<<a_ten.n_dim_<<std::endl;
 
     return 0;
 }
